4f44fcc2784d256eca557b7cdbb8c98181d6aad6
1F10240111 <s1f102401113@iniad.org>
2025-06-26 11:53:01 +0900

    Taskモデルを追加

--- todo/tests.py
+++ todo/tests.py
@@ -1,6 +1,34 @@
 from django.test import TestCase
+from django.utils import timezone
+from datetime import datetime
+from todo.models import Task
 
-# Create your tests here.
-class SampleTestCase(TestCase):
-    def test_sample1(self):
-        self.assertEqual(1 + 2, 3)
+class TaskModelTestCase(TestCase):
+    def test_create_task1(self):
+        due = timezone.make_aware(datetime(2024, 6, 30, 23, 59, 59))
+        task = Task(title='task1', due_at=due)
+        task.save()
+
+        task = Task.objects.get(pk=task.pk)
+        self.assertEqual(task.title, 'task1')
+        self.assertFalse(task.completed)
+        self.assertEqual(task.due_at, due)
+
+    def test_create_task2(self):
+        task = Task(title='task2')
+        task.save()
+
+        task = Task.objects.get(pk=task.pk)
+        self.assertEqual(task.title, 'task2')
+        self.assertFalse(task.completed)
+        self.assertEqual(task.due_at, None)
+
+    def test_create_task3(self):
+        task = Task(title='task3', completed=False)
+        task.save()
+
+        task.completed = True
+        task.save()
+
+        updated = Task.objects.get(pk=task.pk)
+        self.assertTrue(updated.completed)
