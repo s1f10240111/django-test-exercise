4579f7a3b231d0cc2e292b28971ffee346202175
1F10240111 <s1f102401113@iniad.org>
2025-07-02 18:01:41 +0900

    テストを追加

--- todo/tests.py
+++ todo/tests.py
@@ -1,8 +1,24 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 from django.utils import timezone
 from datetime import datetime
 from todo.models import Task
 
+class TodoViewTestCase(TestCase):
+    def test_index_get(self):
+        client = Client()
+        response = client.get('/')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response. templates[0].name, 'todo/index.html')
+        self.assertEqual(len(response.context['tasks']), 0)
+    def test_index_post(self):
+        client = Client()
+        data = {'title': 'Test Task', 'due_at' : '2024-06-30 23:59:59'}
+        response = client.post('/', data)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.templates[0].name, 'todo/index.html' )            
+        self.assertEqual(len(response.context['tasks']), 1)
 class TaskModelTestCase(TestCase):
     def test_create_task1(self):
         due = timezone.make_aware(datetime(2024, 6, 30, 23, 59, 59))
