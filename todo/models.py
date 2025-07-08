270f4242841dda6a1525d50ae1ae155dfded5944
1F10240111 <s1f102401113@iniad.org>
2025-07-08 09:53:01 +0900

    Add detail page and update views, templates, and routing

--- todo/models.py
+++ todo/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 from django.utils import timezone
 
-# Create your models here.
 class Task(models.Model):
     title = models.CharField(max_length=100)
     completed = models.BooleanField(default=False)
     posted_at = models.DateTimeField(default=timezone.now)
     due_at = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return self.title
 
     def is_overdue(self, now=None):
         if self.due_at is None:
--- todo/views.py
+++ todo/views.py
@@ -1,21 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Task
 from django.utils.dateparse import parse_datetime
 from django.utils.timezone import make_aware
 
 def index(request):
     if request.method == 'POST':
-        task = Task(
-            title=request.POST['title'],
-            due_at=make_aware(parse_datetime(request.POST['due_at']))
-        )
-        task.save()
-    if request.GET.get('order') == 'due':
-        tasks = Task. objects. order_by('due_at' )
+        title = request.POST.get('title', '').strip()
+        due_str = request.POST.get('due_at', '')
+        due = make_aware(parse_datetime(due_str)) if due_str else None
+
+        if title:
+            Task.objects.create(title=title, due_at=due)
+
+    order = request.GET.get('order')
+    if order == 'due':
+        tasks = Task.objects.order_by('due_at')
     else:
-        tasks = Task. objects. order_by('-posted_at')    
-    # POSTの後もGETでも共通でタスクリストを取得して表示
-    context = {
-        'tasks': tasks
-    }
-    return render(request, 'todo/index.html', context)
+        tasks = Task.objects.order_by('-posted_at')
+
+    return render(request, 'todo/index.html', {'tasks': tasks})
+
+def detail(request, task_id):
+    task = get_object_or_404(Task, pk=task_id)
+    return render(request, 'todo/detail.html', {'task': task})
