4302441914da4b8635a35e52f5e49cadcb39ef46
1F10240111 <s1f102401113@iniad.org>
2025-07-09 19:03:32 +0900

    詳細ページを追加

--- todo/views.py
+++ todo/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Task
-from django.utils.dateparse import parse_datetime
+from django.shortcuts import render, redirect
+from django.http import Http404
 from django.utils.timezone import make_aware
+from django.utils.dateparse import parse_datetime
+from .models import Task
 
 def index(request):
     if request.method == 'POST':
@@ -11,15 +12,22 @@ def index(request):
 
         if title:
             Task.objects.create(title=title, due_at=due)
+            return redirect('index')  # ←これがないと更新されない！
 
     order = request.GET.get('order')
     if order == 'due':
         tasks = Task.objects.order_by('due_at')
     else:
         tasks = Task.objects.order_by('-posted_at')
-
     return render(request, 'todo/index.html', {'tasks': tasks})
 
 def detail(request, task_id):
-    task = get_object_or_404(Task, pk=task_id)
-    return render(request, 'todo/detail.html', {'task': task})
+    try:
+        task = Task.objects.get(pk=task_id)
+    except Task. DoesNotExist:
+        raise Http404("Task does not exist")
+
+    context = {
+    'task': task,
+    }
+    return render(request, 'todo/detail.html', context)
